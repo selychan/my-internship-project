e872318b686657b71a4b42fe21580f189f5af6f0
selychan <93843083+selychan@users.noreply.github.com>
2024-07-17 23:40:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,12 @@ def vulnerable_view():
     user_input = request.args.get('user_input', '')
     response_html = f"<html><body>User input: {user_input}</body></html>"
     return response_html
+    
+app.route('/greet', methods=['GET'])
+def greet():
+    name = request.args.get('name')
+    greeting = f"<h1>Hello, {name}!</h1>"  # User input is directly included in HTML
+    return greeting
 
 
 if __name__ == '__main__':
